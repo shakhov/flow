57115452b89c300be98708faa4e5b1631f52c59d
Artyom Shakhov <shahov.a.a@gmail.com>
2012-12-14 20:10:15 +0400

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to shakhov.flow
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/shakhov/flow/core_test.clj
@@ -0,0 +1,7 @@
+(ns shakhov.flow.core-test
+  (:use clojure.test
+        shakhov.flow.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
