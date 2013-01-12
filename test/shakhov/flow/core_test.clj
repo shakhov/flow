1d2d63e7d51837b865d7edf798f1920e51a654d4
Artyom Shakhov <shahov.a.a@gmail.com>
2013-01-12 23:53:55 +0400

    Rewrite all core tests

--- test/shakhov/flow/core_test.clj
+++ test/shakhov/flow/core_test.clj
@@ -2,56 +2,154 @@
   (:use clojure.test
         shakhov.flow.core))
 
-(def fnk-1
-  (fnk [a b c] (+ a b c)))
+;; Simple fnk defined using plain argument vector
+(def fnk-vector
+  (fnk [a b c d] (+ a b c d)))
 
-(deftest fnk-test
-  (is (fn? fnk-1))
-  (is (= #{:a :b :c} (:shakhov.flow.core/required-keys (meta fnk-1))))
-  (is (= (+ 4 5 6) (fnk-1 {:a 4 :b 5 :c 6}))))
+(deftest fnk-vector-test
+  ;; fnk is a function
+  (is (fn? fnk-vector))
+  ;; with metadata
+  (is (meta fnk-vector))
+  ;; metadata contains set of required keys
+  (is (= #{:a :b :c :d} (:shakhov.flow.core/required-keys (meta fnk-vector))))
+  ;; fnk is called with single map argument to produce result of its body evaluation
+  (is (= 10 (fnk-vector {:a 1 :b 2 :c 3 :d 4}))))
 
+;; More complex fnk definition using clojure's binding map syntax
+(def fnk-map
+  (fnk {key-a :key-a key-b :key-b                           ; keys
+        :keys [a b] :syms [sym-a sym-b] :strs [str-a str-b] ; special keys
+        :or {key-b 100 sym-b 100 str-b 100 b 100} :as args} ; default values
+       (+ a b key-a key-b sym-a sym-b str-a str-b)))
+
+(deftest fnk-map-test
+  ;; fnk is a function
+  (is (fn? fnk-map))
+  ;; metadata contains both required and optional keys
+  (is (= #{:a :key-a 'sym-a "str-a"} (:shakhov.flow.core/required-keys (meta fnk-map))))
+  (is (= #{:b :key-b 'sym-b "str-b"} (:shakhov.flow.core/optional-keys (meta fnk-map))))
+  ;; fnk called with required keys only
+  (is (= 800 (fnk-map {:a 100 :key-a 100 'sym-a 100 "str-a" 100})))
+  ;; fnk called with required and optional keys
+  (is (= 400 (fnk-map {:a 100 :key-a 100 'sym-a 100 "str-a" 100
+                       :b 0   :key-b 0   'sym-b 0   "str-b" 0}))))
+
+;; Flow is just a map from keywords to fnks. 
+;; Flow can be created as a regular map {key (fnk [args] body)}
+;; or using 'flow' macro with similar syntax {key ([args] body)}
+;; Fnk's inputs are used to determine key dependencies and evaluation order.
 (def flow-1
-  (flow {:x ([a] (+ 10 a))
-         :y ([b] (/ b 5))
-         :z ([x y] (+ x y))}))
+  (flow
+   {:a  ([x] (- x))
+    :b  ([y z] (+ y z))
+    'c  ({:keys [a b alpha]
+          ;; default values for 'a' and 'b' can be defined
+          ;; but these are ignored during flow evaluation
+          :or {a 0 b 0 alpha 1}}
+         (/ b a alpha))
+    "d" ({z :z :syms [c]} (* c z))
+    :e  ({:keys [a] :strs [d]} (+ a d))}))
 
-(def flow-cube
-  (flow {:num-faces ([] 6)
-         :num-edges ([] 12)
-         :face-area ([a] (* a a))
-         :total-area ([face-area num-faces] (* face-area num-faces))
-         :volume ([a] (* a a a))}))
+(deftest fnk-inputs-test
+  ;; defalut values for fnk keys are ignored since flow contains same keys
+  ;; keys :a and :b are required, though default values {a 0 b 0} were defined
+  (is (= #{:a :b} (fnk-inputs flow-1 (flow-1 'c)))))
 
-(deftest flow-test
-  (is (fn? (eager-compile flow-1)))
-  (is (= {:a 10 :b 20 :x 20 :y 4 :z 24}
-         ((eager-compile flow-1) {:a 10 :b 20})))
-  (is (= {:a 0.5 :b -0.6 :x 10.5 :y -0.12 :z 10.38}
-         ((eager-compile flow-1) {:a 0.5 :b -0.6})))
-  (is (= {:a 2.5 :num-faces 6 :num-edges 12 :face-area 6.25 :total-area 37.5 :volume 15.625}
-         ((eager-compile flow-cube) {:a 2.5}))))
+;; Flow can be compiled to 'eager' function which takes input map
+;; and evaluates all flow key values at once.
+(def eager-flow (eager-compile flow-1))
 
-(deftest flow-overload
-  (is (= {:a 0.5 :b -0.6 :x 999 :y -0.12 :z 998.88}
-         ((eager-compile flow-1) {:a 0.5 :b -0.6 :x 999}))))
+(deftest eager-flow-test
+  (is (fn? eager-flow)))
+
+(deftest eager-flow-eval-test
+  ;; Given an input map eager function returns map of all inputs and outputs
+  (is (= {:x 1 :y 2 :z 3 :a -1 :b 5 'c -5 "d" -15 :e -16}
+         (eager-flow {:x 1 :y 2 :z 3})))
+  (is (= {:x 4 :y 5 :z 6 :a -4 :b 11 'c -11/4 "d" -33/2 :e -41/2}
+         (eager-flow {:x 4 :y 5 :z 6}))))
+
+(deftest eager-flow-eval-override-test
+  ;; Some keys can be overridden by input map keys.
+  ;; Overridden value is used to evaluate dependent keys.
+  (is (= {:x 1 :y 2 :z 3 :a -1 :b 5 'c 100 "d" 300 :e 299}
+         (eager-flow {:x 1 :y 2 :z 3 'c 100}))))
+
+;; Atom for lazy evaluation testing
+;; FIXME: single atom for multiple tests can probably cause test fails
+(def flow-log (atom #{}))
+
+;; Each time a key is evaluated, it is pushed into log with this helper function
+(defn- log-key [key]
+  (swap! flow-log #(into % #{key})))
+
+;; Same flow as above with some simple logging
+(def flow-2
+    (flow
+     {:a  ([x] (log-key :a)
+             (- x))
+      :b  ([y z] (log-key :b)
+             (+ y z))
+      'c  ({:keys [a b] :or {a 0 b 0}}
+           (log-key 'c)
+           (/ b a))
+      "d" ({z :z :syms [c]}
+           (log-key "d")
+           (* c z))
+      :e  ({:keys [a] :strs [d]}
+           (log-key :e)
+           (+ a d))}))
+
+;; Flow function can be compiled to 'lazy' function returning 'lazy-map'
+;; 'Lazy' function is called just like an 'eager' one.
+;; Keys of a lazy map are evaluated only once when the value is needed.
+(def lazy-flow (lazy-compile flow-2))
 
 (deftest lazy-flow-test
-  (is (fn? (lazy-compile flow-1)))
-  (is (= 24 (:z ((lazy-compile flow-1) {:a 10 :b 20})))))
-  (is (= 15.625 (:volume ((lazy-compile flow-cube) {:a 2.5}))))
+  (is (fn? lazy-flow)))
 
-(def fnk-2
-  (fnk {:keys [a b]
-        :strs [str-a str-b]
-        :syms [sym-a sym-b]
-        key-a :keyA
-        key-b :keyB
-        :or {a -10 sym-a -20 str-a -30 key-a -40}}
-       (+ a b sym-a sym-b str-a str-b key-a key-b)))
+(deftest lazy-flow-eval-test
+  (reset! flow-log #{})
+  ;; evaluate single key
+  (is (= 11 ((lazy-flow {:x 4 :y 5 :z 6}) :b)))
+  ;; only :b has been evaluated
+  (is (= #{:b} @flow-log))
+  ;; reset log
+  (reset! flow-log #{})
+  ;; evaluate 'c key
+  (is (= -11/4 ((lazy-flow {:x 4 :y 5 :z 6}) 'c)))
+  ;; :a :b 'c are needed to get 'c value
+  ;; notice: :b has been evaluated again,
+  ;; because call to the lazy-flow created new lazy-map
+  (is (= #{:a :b 'c} @flow-log)))
 
-(deftest fnk-binding
-  (is (= #{:b 'sym-b "str-b" :keyB} (:shakhov.flow.core/required-keys (meta fnk-2))))
-  (is (= #{:a 'sym-a "str-a" :keyA} (:shakhov.flow.core/optional-keys (meta fnk-2))))
-  (is (= 0 (fnk-2 {:b 10 "str-b" 20 'sym-b 30 :keyB 40})))
-  (is (= 200 (fnk-2 {:b 10 "str-b" 20 'sym-b 30 :keyB 40
-                     :a 10 "str-a" 20 'sym-a 30 :keyA 40}))))
+;; Input map keys can override outputs
+;; When overridden key acts as a bridge in a flow graph
+;; keys in a separated sucomponent are ignored
+(deftest lazy-flow-override-test
+  (reset! flow-log #{})
+  (is (= #{} @flow-log))
+  ;; override 'c
+  (is (= 596 ((lazy-flow {:x 4 :y 5 :z 6 'c 100}) :e)))
+  ;; :b key is not required, since 'c is overridden
+  (is (= #{:a "d" :e} @flow-log)))
+
+;; Call a compilled lazy flow function to produce lazy map
+;; None of its keys are evaluated at this timex
+(def lazy-flow-map (lazy-flow {:x 1 :y 2 :z 3}))
+
+;; Key values in a lazy map are evaluated and memoized first time they're needed.
+(deftest lazy-flow-map-test
+  (reset! flow-log #{})
+  ;; get the :b key value
+  (is (= 5 (:b lazy-flow-map)))
+  ;; only :b key was evaluated
+  (is (= #{:b} @flow-log))
+  ;; now reset the log
+  (reset! flow-log #{})
+  ;; get 'c' key value
+  (is (= -5 ('c lazy-flow-map)))
+  ;; only :a and 'c keys were evaluated, but not :b
+  ;; each key function is called only once since in the lazy-map
+  (is (= #{:a 'c} @flow-log)))
